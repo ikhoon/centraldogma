5521b20800a8ab3427ac3efe0ead255983283d92
Ikhun Um <ih.pert@gmail.com>
2024-04-23 17:07:19 +0900

    try another workaround

--- server/src/test/java/com/linecorp/centraldogma/server/management/ServerStatusManagerIntegrationTest.java
+++ server/src/test/java/com/linecorp/centraldogma/server/management/ServerStatusManagerIntegrationTest.java
@@ -87,8 +87,6 @@ class ServerStatusManagerIntegrationTest {
         assertThatThrownBy(() -> getServerStatus(client))
                 .isInstanceOf(UnprocessedRequestException.class)
                 .hasCauseInstanceOf(ConnectException.class);
-        // Wait for the ports acquired to be released.
-        Thread.sleep(5000);
 
         // Restart the cluster with the same configuration.
         cluster.start();
@@ -113,8 +111,6 @@ class ServerStatusManagerIntegrationTest {
         assertThatThrownBy(() -> getServerStatus(client))
                 .isInstanceOf(UnprocessedRequestException.class)
                 .hasCauseInstanceOf(ConnectException.class);
-        // Wait for the ports acquired to be released.
-        Thread.sleep(5000);
 
         cluster.start();
         serverStatus = getServerStatus(client);
